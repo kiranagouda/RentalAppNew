bfa2a1673b133bd1c168cae3fefe42a86abd37d4
kiranagouda <kirangouda65@gmail.com>
2023-07-13 18:00:32 +0530

    commit 5

--- src/test/java/btm/Example.java
+++ src/test/java/btm/Example.java
@@ -2,15 +2,30 @@ package btm;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Example {
 		@Test
-		public void example() {
-		   WebDriverManager.chromedriver().setup();
-		      WebDriver driver=new ChromeDriver();
+		public void example(WebDriver driver,String BROWSER) {
+		  // WebDriverManager.chromedriver().setup();
+		   //   WebDriver driver=new ChromeDriver();
+		      
+		    //welcome
+				if(BROWSER.equalsIgnoreCase("chrome")) {
+					WebDriverManager.chromedriver().setup();
+				    driver = new ChromeDriver();
+				}else if(BROWSER.equalsIgnoreCase("firefox")) {
+					//WebDriverManager.firefoxdriver().setup();
+					//driver = new FirefoxDriver();
+				}else if(BROWSER.equalsIgnoreCase("edge")) {
+					driver = new EdgeDriver();
+				}else {
+				    driver = new ChromeDriver();
+				}
+		      
 		      driver.get("https://www.myntra.com");
 }
 }
--- src/test/java/btm/LoginPage.java
+++ src/test/java/btm/LoginPage.java
@@ -2,18 +2,32 @@ package btm;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginPage {
 		@Test
-	public void login() {
+	public void login(WebDriver driver,String BROWSER) {
 			//welcome
 			
 		//public static void main(String[] args) {
-	      WebDriverManager.chromedriver().setup();
-	      WebDriver driver=new ChromeDriver();
+	    //  WebDriverManager.chromedriver().setup();
+	      //WebDriver driver=new ChromeDriver();
+	      
+	      if(BROWSER.equalsIgnoreCase("chrome")) {
+				WebDriverManager.chromedriver().setup();
+			    driver = new ChromeDriver();
+			}else if(BROWSER.equalsIgnoreCase("firefox")) {
+				//WebDriverManager.firefoxdriver().setup();
+				//driver = new FirefoxDriver();
+			}else if(BROWSER.equalsIgnoreCase("edge")) {
+				driver = new EdgeDriver();
+			}else {
+			    driver = new ChromeDriver();
+			}
+	      
 	      driver.get("https://www.amazon.com");
 	
 	      
